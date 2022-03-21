d8723608601733ab72dcac4d4aec7caca93f70df
Andrés Triana <wtriana@outlook.com>
2022-03-20 23:14:29 -0500

    Add get request

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using NetBookShop.Data;
+using NetBookShop.Models;
 
 namespace NetBookShop.Controllers
 {
     public class CategoryController : Controller
     {
+        // Read of my categories
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+        // Basic view of category index page
         public IActionResult Index()
         {
-            return View();
+            // var categoryList = _db.Categories.ToList();
+            IEnumerable<Category> categoryList = _db.Categories;
+            return View(categoryList);
         }
     }
 }
